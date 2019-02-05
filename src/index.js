0de388106a1c47b50a5aafc29d5e2022bc545b80
Leonardo Brito <f.leobrito@gmail.com>
2019-02-05 17:10:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+/**
+ * Database setup
+ */
+
+mongoose.connect(
+  "mongodb://goweek:goweek123@ds149207.mlab.com:49207/goweek-backend",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
+app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(require("./routes"));
+
+app.listen("3000", () => {
+  console.log("Server started on port 3000");
+});
